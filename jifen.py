bc1a578bb1fe7334bb21b0c1cfc8861b6a435199
simannb <simannb@notebook.com>
2018-01-06 23:05:29 +0800

    version 0.1

--- jifen.py
+++ jifen.py
@@ -8,40 +8,93 @@
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from bs4 import BeautifulSoup
-import requests
 import time
 
-url = "http://liansai.500.com/zuqiu-4429/jifen-11734"
 
-browser = webdriver.Chrome()
-browser.get(url)
-teamList = browser.find_element_by_id("one_team_select")
-Select(teamList).select_by_value("1072")
-time.sleep(2)
-#score = browser.find_element_by_id("match_list_tbody")
-#print(browser.page_source)
-#print(score.text)
-#headers = {
-#'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36'
-#}
-#html = requests.get(url, headers=headers).content.decode("gbk")
-##print(html)
-#
+def getPageSource(url, teamCode):
+    # 进入浏览器设置
+    options = webdriver.ChromeOptions()
+    # 设置中文
+    options.add_argument('lang=zh_CN.UTF-8')
+    # 更换头部
+    options.add_argument(
+        'user-agent="Mozilla/5.0 (iPod; U; CPU iPhone OS 2_1 like Mac OS X; ja-jp) AppleWebKit/525.18.1 '
+        '(KHTML, like Gecko) Version/3.1.1 Mobile/5F137 Safari/525.20"')
+    browser = webdriver.Chrome(chrome_options=options)
+    browser.get(url)
+    teamList = browser.find_element_by_id("one_team_select")
+    Select(teamList).select_by_value(teamCode)
+    time.sleep(3)
+    pageSource = browser.page_source
+    browser.quit()
+    return pageSource
 
-html = browser.page_source
 
-soup = BeautifulSoup(html, "html5lib")
+def analyseDataFromPageSource(html):
+    """
+    :return: a list
+    """
+    soup = BeautifulSoup(html, "html5lib")
 
-title = soup.title.text
-print(title)
+    scoreTable = soup.find("div", id="season_match_list").find("table")
+    tbody = scoreTable.find("tbody")
+    dataList = [('轮次', '比赛时间', '主队', '比分', '客队', '平均欧指', '赛果', '澳门盘口',  '盘路','大小', '历史交战')]
+    for tr in tbody.find_all("tr"):
+        tmpList = []
+        for td in tr.find_all("td"):
+            if td.getText():
+                tmpList.append(td.getText())
+        while len(tmpList) < 12:
+            tmpList.insert(-2, "")
+        tmpList.pop()
+        if "." in tmpList[6]:
+            _tmp = tmpList[6].split(".")
+            first = "{}.{}".format(_tmp[0], _tmp[1][:2])
+            second = "{}.{}".format(_tmp[1][2:], _tmp[2][:2])
+            third = "{}.{}".format(_tmp[2][2:], _tmp[3])
+            del tmpList[6]
+            tmpList.insert(5, "{} {} {}".format(first, second, third))
+        dataList.append(tuple(tmpList))
+    return dataList
 
-scoreTable = soup.find("div", id="season_match_list").find("table")
-#print(scoreTable)
-tbody = scoreTable.find("tbody")
-#print(tbody)
-for tr in tbody.find_all("tr"):
-    print("#" * 100)
-    for td in tr.find_all("td"):
-        if td.getText():
-            print(td.getText())
-            #print(td.get_attribute("class"))
+
+def generateExcel(data, teamName):
+    import csv
+    fileName = "{}.csv".format(teamName)
+    with open(fileName, "w", newline="", encoding="utf-8") as c:
+        cw = csv.writer(c, dialect="excel")
+        cw.writerows(data)
+
+
+
+
+if __name__ == "__main__":
+    url = "http://liansai.500.com/zuqiu-4429/jifen-11734"
+    teamDict = {
+        "1072": "曼  城",
+        "1075": "曼  联",
+        "1173": "切尔西",
+        "1011": "利物浦",
+        "1238": "热  刺",
+        "554": "阿森纳",
+        "700": "伯恩利",
+        "973": "莱切城",
+        "565": "埃弗顿",
+        "1274": "沃特福",
+        "847": "哈德斯",
+        "721": "布赖顿",
+        "1137": "纽  卡",
+        "516": "水晶宫",
+        "1286": "西汉姆",
+        "667": "伯恩茅",
+        "1128": "南安普",
+        "1197": "斯托克",
+        "1284": "西布罗",
+        "511": "斯旺西"
+    }
+
+    for teamCode, teamName in teamDict.items():
+        html = getPageSource(url, teamCode)
+        data = analyseDataFromPageSource(html)
+        generateExcel(data, teamName)
+        time.sleep(3)
